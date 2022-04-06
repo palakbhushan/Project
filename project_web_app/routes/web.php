891d242859bfbef4af3f062b4cb05b6982fea5d9
palak bhushan <palakbosw@gmail.com>
2022-04-06 21:52:13 +0530

    setting up

---
+++ project_web_app/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// use App\Http\controllers\BookController;
+use App\Http\controllers\BookController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/',[BookController::class,'index']);
+
+Route::get('index',function(){
+    return view('index');
+});
+
+Route::get('home',function(){
+    return view('home');
+});
+
+
+Route::get('product',function(){
+    return view('product');
+});
